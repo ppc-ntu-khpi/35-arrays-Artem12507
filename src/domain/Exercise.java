ae715de3b8b4a6844595ce5b919ec00b006fb983
Artem12507 <59194216+Artem12507@users.noreply.github.com>
2021-02-13 15:15:23 +0200

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,36 @@
 package domain;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
+public class Exercise 
+{	
+	private static final ThreadLocalRandom rnd = ThreadLocalRandom.current();
+	
+	public static int[] creature(int size) 
+        {
+		int[] array = new int[size];
+		for (int i = 0; i < size; i++) 
+                {
+			array[i] = rnd.nextInt(10);
+		}
+		return array;
+	}	
+	
+	public static int reveal(int[] arr) 
+        {
+		Map<Integer, Integer> nums = new HashMap<>();
+		for (int number : arr) {
+			Integer i = nums.get(number);
+			nums.put(number, i == null ? 1 : i+1);
+		}		
+		int max = Collections.max(nums.values());
+		for (Map.Entry<Integer, Integer> number : nums.entrySet()) 
+                {
+			if (number.getValue() == max)
+				return number.getKey();
+		}
+		return -1;
+	}
 }
